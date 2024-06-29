8a69c12cf3d7e9379cc081d2678641e3d0397bd5
prem-y <raopremwaghmare@gmail.com>
2024-06-29 19:47:32 +0530

    updates

--- Exp2.java
+++ Exp2.java
@@ -1,40 +1,47 @@
+/*
+ * Find the bigram count for the given corpus. Apply Laplace smoothing and find the bigram probabilities after add-one smoothing (up to 4 decimal places)
+ */
+
 public class Exp2 {
-   
-    public static int bigramCount(String[] corpus, String word1, String word2){
+
+    public static int bigramCount(String[] corpus, String word1, String word2) {
         int count = 0;
-        for(int i=0;i<corpus.length;i++){
-            String[]  corpusTokens = corpus[i].split(" ");
-            for(int j=0;j<corpusTokens.length-1;j++){
-                if(word1.equals(corpusTokens[j]) && word2.equals(corpusTokens[j+1])){
+        for (int i = 0; i < corpus.length; i++) {
+            String[] corpusTokens = corpus[i].split(" ");
+            for (int j = 0; j < corpusTokens.length - 1; j++) {
+                if (word1.equalsIgnoreCase(corpusTokens[j]) && word2.equals(corpusTokens[j + 1])) {
                     count++;
                 }
             }
         }
         return count;
     }
-    public static int unigramCount(String[] corpus, String word){
+
+    public static int unigramCount(String[] corpus, String word) {
         int count = 0;
-        for(int i=0;i<corpus.length;i++){
-            String[]  corpusTokens = corpus[i].split(" ");
-            for(int j=0;j<corpusTokens.length-1;j++){
-                if(word.equals(corpusTokens[j])){
+        for (int i = 0; i < corpus.length; i++) {
+            String[] corpusTokens = corpus[i].split(" ");
+            for (int j = 0; j < corpusTokens.length - 1; j++) {
+                if (word.equalsIgnoreCase(corpusTokens[j])) {
                     count++;
                 }
             }
         }
         return count;
     }
-   
+
     public static void main(String[] args) {
-        String[] corpus = {"I like to play cricket", "I play on sunny day", "I like to play chess"};
-        String test = "I like football";
+        String[] corpus = { "There is a big garden", "Children play in the garden",
+                "They play inside beautiful garden" };
+        String test = "They play in a big garden";
         String[] testTokens = test.split(" ");
         double probability = 1;
-        int corpusSize= 9;
-        for(int i=0; i<testTokens.length-1;i++){
-            probability *= (double) (bigramCount(corpus, testTokens[i], testTokens[i+1])+1)/(unigramCount(corpus,testTokens[i])+ corpusSize);
+        int corpusSize = 9;
+        for (int i = 0; i < testTokens.length - 1; i++) {
+            probability *= (double) (bigramCount(corpus, testTokens[i], testTokens[i + 1]) + 1)
+                    / (unigramCount(corpus, testTokens[i]) + corpusSize);
         }
-       
-        System.out.format("Probability: %.4f",probability);
+
+        System.out.format("Probability: %.4f", probability);
     }
 }
