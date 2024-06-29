b8d742793b3afe81ea629dd8e17229d51a6af933
prem-y <raopremwaghmare@gmail.com>
2024-06-29 19:41:22 +0530

    updates

--- Exp1.java
+++ Exp1.java
@@ -1,3 +1,11 @@
+/*
+Consider the following Corpus of three sentences
+a)	There is a big garden.
+b)	Children play in a garden
+c)	They play inside beautiful garden
+Calculate P for the sentence “They play in a big Garden” assuming a bi-gram language model.
+ */
+
 public class Exp1 {
 
     public static int bigramCount(String[] corpus, String word1, String word2) {
@@ -5,7 +13,7 @@ public class Exp1 {
         for (int i = 0; i < corpus.length; i++) {
             String[] corpusTokens = corpus[i].split(" ");
             for (int j = 0; j < corpusTokens.length - 1; j++) {
-                if (word1.equals(corpusTokens[j]) && word2.equals(corpusTokens[j + 1])) {
+                if (word1.equalsIgnoreCase(corpusTokens[j]) && word2.equals(corpusTokens[j + 1])) {
                     count++;
                 }
             }
@@ -18,7 +26,7 @@ public class Exp1 {
         for (int i = 0; i < corpus.length; i++) {
             String[] corpusTokens = corpus[i].split(" ");
             for (int j = 0; j < corpusTokens.length - 1; j++) {
-                if (word.equals(corpusTokens[j])) {
+                if (word.equalsIgnoreCase(corpusTokens[j])) {
                     count++;
                 }
             }
@@ -27,14 +35,14 @@ public class Exp1 {
     }
 
     public static void main(String[] args) {
-        String[] corpus = { "I like to play cricket", "I play on sunny day", "I like to play chess" };
-        String test = "I like football";
+        String[] corpus = { "There is a big garden", "Children play in a garden", "They play inside beautiful garden" };
+        String test = "They play in a big Garden";
         String[] testTokens = test.split(" ");
         double probability = 1;
         int corpusSize = 9;
         for (int i = 0; i < testTokens.length - 1; i++) {
             probability *= (double) (bigramCount(corpus, testTokens[i], testTokens[i + 1]))
-                    / (unigramCount(corpus, testTokens[i]));
+                    / (unigramCount(corpus, testTokens[i])+corpusSize);
         }
 
         System.out.format("Probability: %.4f", probability);
